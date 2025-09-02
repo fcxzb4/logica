5719c24bd70e33a486063bd5f0fa09e164ca5401
Bridget <84099228+fcxzb4@users.noreply.github.com>
2025-09-01 21:52:59 -0300

    fdsf

--- laçosrepetição.js
+++ laçosrepetição.js
@@ -1,20 +1,52 @@
+// const senha = 'abacaju'
+// let tentativas = 3
+// let senha_dg
+
+// do{
+//     senha_dg = input('tente seu insolente:  ')
+//     tentativas--;
+//     if( senha_dg != senha ){
+//         console.log(`Senha incorreta. Você tem ${tentativas} tentativa(s) restante(s).`)
+//     }
+// }while(senha_dg != senha && tentativas > 0)
+//     if(senha_dg == senha ){
+//         console.log("voce acertou ")
+//     }else{
+//         console.log("voce e uma bixona")
+//     }
+
+
+// function calcularFatorial(amen) {
+//   let resultado = 1;
+//   for (let i = amen; i > 0; i--) {
+//     resultado = resultado * i;
+//   }
+//   return resultado;
+// }
+
+// let user;
+// do{
+//     user = parseInt(input("digite numero:  "))
+// }while(user <= 0);
+
+// console.log(`resultado do fatorial ${amen} é ${calcularFatorial(amen)} `)
+
 const input = require("prompt-sync")();
 
+function analisarNumero(pnc){
+     if(pnc/2 == 0){
+        console.log("receba pai par")
+     }
+     if(pnc/2 == 1){
+        console.log("receba pai impar")
+     };
+     if(pnc < 0){
+       console.log("receba pai negativo")
+     }
+     else{
+        console.log("receba pai positivo")
+     }
+} 
 
 
-const senha = 'abacaju'
-let tentativas = 3 
-let senha_dg 
-
-do{
-    senha_dg = input('tente seu insolente:  ')
-    tentativas--;
-    if( senha_dg != senha ){
-        console.log(`Senha incorreta. Você tem ${tentativas} tentativa(s) restante(s).`)
-    }
-}while(senha_dg != senha && tentativas > 0) 
-    if(senha_dg == senha ){
-        console.log("voce acertou ")
-    }else{
-        console.log("voce e uma bixona")
-    }
+console.log("receba pai")
